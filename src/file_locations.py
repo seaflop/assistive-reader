c7978b4cd492281ed6aaf7fe5da12acd93520f83
seaflop <179542068+seaflop@users.noreply.github.com>
2026-01-10 03:27:19 -0500

    Refer to previous commit, forgot to add this file

--- src/file_locations.py
+++ src/file_locations.py
@@ -1,6 +1,7 @@
 import os
 
 main_script_path = os.path.join(".", "src", "main.py")
+realmain_script_path = os.path.join(".", "src", "realmain.py")
 
 tts_path = os.path.join(".", "tts", "tts1.wav")
 image_path = os.path.join(".", "images", "example2.jpg")
@@ -11,3 +12,5 @@ voice_path = os.path.join(".", "voices", "en_US-danny-low.onnx")
 ready_sound_path = os.path.join(".", "sound_effects", "ready.wav")
 pause_sound_path = os.path.join(".", "sound_effects", "pause.wav")
 resume_sound_path = os.path.join(".", "sound_effects", "resume.wav")
+idling_sound_path = os.path.join(".", "sound_effects", "idling.wav")
+camera_sound_path = os.path.join(".", "sound_effects", "idling.wav")

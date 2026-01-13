6081689465370a774a01d57aa2b97cb0a1768c4f
seaflop <179542068+seaflop@users.noreply.github.com>
2026-01-13 15:09:46 -0500

    Added webcam functionality
    Added a function in the Capstone class that opens the webcam and allows
    the user to take a picture.

--- src/capstone.py
+++ src/capstone.py
@@ -1,6 +1,47 @@
 from tts import TTS
 from ocr import OCR
+import cv2
 
 class Capstone(TTS, OCR):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+    
+    def take_picture(self, image_location: str):
+        
+        camera = cv2.VideoCapture(0)
+        if (not camera.isOpened()):
+            raise OSError("Error: Could not open the webcam")
+        
+        while True:
+            # Capture frame-by-frame
+            ret, frame = camera.read()
+
+            if not ret:
+                print("Error: Could not read frame.")
+                break
+
+            # Display the resulting frame
+            cv2.imshow('Webcam Feed - Press s to Save, q to Quit', frame)
+
+            # Wait for a key press
+            key = cv2.waitKey(1) & 0xFF
+
+            # If 's' is pressed, save the image
+            if key & 0xFF == ord(" "):
+                image_name = image_location
+                cv2.imwrite(image_name, frame)
+                break
+            
+            # If 'q' is pressed, quit the application
+            elif key & 0xFF == 27:
+                break
+
+        # When everything is done, release the capture and destroy windows
+        camera.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    import file_locations as fl
+    c = Capstone(path_to_voice = fl.voice_path)
+    c.take_picture("./images/image.jpg")
+

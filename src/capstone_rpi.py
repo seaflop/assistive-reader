7dbc1b83da76fedc1222c6a9053856e4fee60261
seaflop <179542068+seaflop@users.noreply.github.com>
2026-01-20 11:29:37 -0500

    added raspberry pi camera initialization and capture to file functionality

--- src/capstone_rpi.py
+++ src/capstone_rpi.py
@@ -5,9 +5,8 @@ from picamera2 import Picamera2
 class Capstone(TTS, OCR):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._camera = Picamera2()
+        self._camera.start()
     
     def take_picture(self, image_file_location: str):
-        camera = Picamera2()
-        camera.start()
-        camera.capture_file(image_file_location)
-        camera.stop()
+        self._camera.capture_file(image_file_location)
